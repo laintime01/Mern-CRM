c02560ffb9c55823427bf897a3d47dac5b489897
ZHENG HAO <hzhe111@163.com>
2024-08-18 08:19:11 -0400

    setup backend structure

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const connectDB = require('./config/db');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+
+// Connect Database
+connectDB();
+
+// Init Middleware
+app.use(express.json({ extended: false }));
+app.use(cors());
+
+// Define Routes
+app.use('/api/users', require('./routes/users'));
+app.use('/api/clients', require('./routes/clients'));
+app.use('/api/service-requests', require('./routes/serviceRequests'));
+app.use('/api/tasks', require('./routes/tasks'));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
